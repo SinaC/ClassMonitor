c9de8b05165467496ad6e9447b42c1338b4713ce
Joel Heymbeeck <heymbeeck.joel@gmail.com>
2011-12-03 08:58:05 +0100

    Config modified
    Signed-off-by: Joel Heymbeeck <heymbeeck.joel@gmail.com>

--- Tukui_ClassMonitor/config/profiles.lua
+++ Tukui_ClassMonitor/config/profiles.lua
@@ -1,14 +1,15 @@
 local T, C, L = unpack(Tukui) -- Import: T - functions, constants, variables; C - config; L - locales
 
 if T.myname == "Meuhhnon" then
-	C["classmonitor"]["DRUID"][1].anchor = nil
+	C["classmonitor"]["DRUID"][1].anchor = nil -- RESOURCE
 	C["classmonitor"]["DRUID"][1].anchors = {
-		{ "CENTER", UIParent, "CENTER", 0, -120 }, -- Balance
-		{ "CENTER", UIParent, "CENTER", 0, -120 }, -- Feral
-		{ "CENTER", UIParent, "CENTER", -500, 290 } -- Restoration
+		{ "CENTER", UIParent, "CENTER", 0, -140 }, -- Balance
+		{ "CENTER", UIParent, "CENTER", 0, -140 }, -- Feral
+		--{ "CENTER", UIParent, "CENTER", -500, 290 } -- Restoration
+		{ "CENTER", UIParent, "CENTER", 0, -140 } -- Restoration
 	}
 elseif T.myname == "Enimouchet" then
-	C["classmonitor"]["PALADIN"][1].anchor = nil
+	C["classmonitor"]["PALADIN"][1].anchor = nil -- RESOURCE
 	C["classmonitor"]["PALADIN"][1].anchors = {
 		{"CENTER", UIParent, "CENTER", -543, 290}, -- Holy
 		{"CENTER", UIParent, "CENTER", -0, -100}, -- Protection
